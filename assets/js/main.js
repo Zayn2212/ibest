dd662aa8bb41e670f8b0276538d3c4f3ba6f9c4c
Zayn2212 <157130452+Zayn2212@users.noreply.github.com>
2024-04-26 23:28:49 +0500

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
     $(window).on("load", function () {
         setTimeout(function () {
             $("#preloader-active").fadeOut("slow");
-        }, 0); // 5000 milliseconds = 5 seconds
+        }, 2500); // 5000 milliseconds = 5 seconds
     });
 
     /*-----------------
